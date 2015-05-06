8a3ccb6c0e1689d672efc5817c0753f415925011
Jeremy Morgan <jeremymorgan@gmail.com>
2015-05-06 14:52:58 -0700

    added a working project

---
+++ WorkingZza/Zza.Services/ZzaService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+
+namespace Zza.Services
+{
+	[ServiceBehavior (InstanceContextMode = InstanceContextMode.PerCall)]
+
+	class ZzaService : IZzaService, IDisposable
+	{
+		readonly ZzaDbContext _Context = new ZzaDbContext();
+
+		public List<Entities.Product> GetProducts()
+		{
+			return _Context.Products.ToList();
+		}
+
+		public List<Entities.Customer> GetCustomers()
+		{
+			return _Context.Customers.ToList();
+		}
+
+		[OperationBehavior(TransactionScopeRequired = true)]
+		public void SubmitOrder(Entities.Order order)
+		{
+			_Context.Orders.Add(order);
+			order.OrderItems.ForEach(oi => _Context.OrderItems.Add(oi));
+			_Context.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			_Context.Dispose();
+		}
+	}
+}
