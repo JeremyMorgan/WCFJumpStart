28b6136b290078ea03cb4819d70f59725f591fbc
Jeremy Morgan <jeremymorgan@gmail.com>
2015-05-07 13:33:44 -0700

    IIS and console host

--- WorkingZza/Zza.IISHost/Properties/AssemblyInfo.cs
+++ WorkingZza/Zza.IISHost/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Zza.SelfHost")]
+[assembly: AssemblyTitle("Zza.IISHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eid Passport, Inc.")]
-[assembly: AssemblyProduct("Zza.SelfHost")]
+[assembly: AssemblyProduct("Zza.IISHost")]
 [assembly: AssemblyCopyright("Copyright © Your Company, Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b552209b-bc8c-458c-b5dc-3441fec7caae")]
+[assembly: Guid("e9ba61fe-fc3a-432f-8427-fbf4c9036358")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- WorkingZza/Zza.Services/Properties/AssemblyInfo.cs
+++ WorkingZza/Zza.Services/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Eid Passport, Inc.")]
 [assembly: AssemblyProduct("Zza.Services")]
-[assembly: AssemblyCopyright("Copyright © Eid Passport, Inc. 2015")]
+[assembly: AssemblyCopyright("Copyright © Your Company, Inc. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
